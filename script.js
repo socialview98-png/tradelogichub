0896c151c2c1ee66bc85dd1882293e2396236d78
socialview98-png <socialview98@gmail.com>
2026-07-27 18:56:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,36 @@ navLinks.querySelectorAll('a').forEach((link) => {
     menuToggle.innerHTML = '<i class="fa-solid fa-bars"></i>';
   });
 });
+
+function calculateRR(){
+
+let entry = Number(document.getElementById("entry").value);
+
+let stoploss = Number(document.getElementById("stoploss").value);
+
+let target = Number(document.getElementById("target").value");
+
+
+if(!entry || !stoploss || !target){
+
+alert("Please enter all values");
+
+return;
+
+}
+
+
+let risk = Math.abs(entry - stoploss);
+
+let reward = Math.abs(target - entry);
+
+let ratio = reward / risk;
+
+
+document.getElementById("risk").innerHTML = risk.toFixed(2);
+
+document.getElementById("reward").innerHTML = reward.toFixed(2);
+
+document.getElementById("ratio").innerHTML = "1 : " + ratio.toFixed(2);
+
+}
