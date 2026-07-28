c59c3a35f166ed6054bff24c621134a898137ef2
socialview98-png <socialview98@gmail.com>
2026-07-28 22:02:08 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,36 +1,56 @@
-function calculateRR(){
+function premiumRR(){
 
 let entry = Number(document.getElementById("entry").value);
-let stop = Number(document.getElementById("stoploss").value);
+let stop = Number(document.getElementById("stop").value);
 let target = Number(document.getElementById("target").value);
 let quantity = Number(document.getElementById("quantity").value);
 
 
-let risk = entry - stop;
-let reward = target - entry;
+let riskPerShare = entry - stop;
+let rewardPerShare = target - entry;
 
 
-let riskAmount = risk * quantity;
-let rewardAmount = reward * quantity;
+let riskAmount = riskPerShare * quantity;
+let rewardAmount = rewardPerShare * quantity;
 
 
-let ratio = reward / risk;
+let ratio = rewardAmount / riskAmount;
 
 
-document.getElementById("risk").innerHTML =
-"Risk Per Share: $" + risk.toFixed(2);
+document.getElementById("riskAmount").innerHTML =
+"$" + riskAmount.toFixed(2);
 
 
-document.getElementById("reward").innerHTML =
-"Reward Per Share: $" + reward.toFixed(2);
+document.getElementById("rewardAmount").innerHTML =
+"$" + rewardAmount.toFixed(2);
 
 
 document.getElementById("ratio").innerHTML =
-"Risk Reward Ratio: 1 : " + ratio.toFixed(2);
+"1 : " + ratio.toFixed(2);
 
 
-document.getElementById("amount").innerHTML =
-"Risk Amount: $" + riskAmount.toFixed(2) +
-"<br>Reward Amount: $" + rewardAmount.toFixed(2);
+
+let quality="";
+
+
+if(ratio >= 3){
+
+quality="Excellent Setup ⭐⭐⭐";
+
+}
+else if(ratio >= 2){
+
+quality="Good Setup ⭐⭐";
+
+}
+else{
+
+quality="Low Risk Reward Setup";
+
+}
+
+
+document.getElementById("quality").innerHTML=quality;
+
 
 }
